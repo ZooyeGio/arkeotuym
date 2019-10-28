dcb6dd93c3d116a7dcfa96a4c02eaa01ee6027f0
ZooyeGio <kte.ent@gmail.com>
2019-10-28 22:28:01 +0930

    Updated settings test

--- arkeotuim/settings.py
+++ arkeotuim/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
+DEBUG = bool( os.environ.get('DEBUG_VALUE', True) )
 
 ALLOWED_HOSTS = ['arkeotuym.herokuapp.com']
 
