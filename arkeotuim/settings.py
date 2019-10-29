6ad3f824d64bcba5288abf566a635551cbb51c77
ZooyeGio <kte.ent@gmail.com>
2019-10-30 01:36:27 +0930

    Updated Settings

--- arkeotuim/settings.py
+++ arkeotuim/settings.py
@@ -148,6 +148,6 @@ LOGIN_URL = 'login'
 # AWS_S3_FILE_OVERWRITE = False
 # AWS_DEFAULT_ACL = None
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 django_heroku.settings(locals())
